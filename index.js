51523e8d62f6ebdfb3286d27eb830a75b4f04b70
aledev <alexmendezz.3011@gmail.com>
2022-05-10 09:43:12 -0400

    Archivos de Heroku

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post('/api/notes', (request, response) => {
   response.json(newNote)
 });
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`);
 })
